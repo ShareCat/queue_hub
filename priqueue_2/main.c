58e1f1d5079ac57a87be31e93bfc0ed3acac8d94
Cat <843553493@qq.com>
2021-09-24 16:19:43 +0800

    [update]

--- priqueue_2/main.c
+++ priqueue_2/main.c
@@ -6,6 +6,10 @@
 #include <limits.h>
 #include "priqueue.h"
 
+#if 0
+/*
+    单线程简单测试
+*/
 int main(int argc, char** argv)
 {
 
@@ -59,3 +63,107 @@ int main(int argc, char** argv)
 
 }
 
+#else
+
+/*
+    多线程测试
+*/
+
+/*
+ * Test code
+ *   Use two threads, acting as producer and consumer
+ */
+void* producer(void* p)
+{
+    ASSERT(p);
+
+    ptable *table = (ptable*)p;
+
+    printf("Thread producer\n");
+    int i = 0;
+
+    while (1) {
+        /*
+         * We break the producer after enqueuing 16 messages
+         */
+        if (i == 16) {
+            break;
+        }
+
+        char *val = malloc(32);
+
+        memset(val, 0x00, 32);
+        sprintf(val, "number %d", i);
+        printf("---> put_data: %s priority: %d \n", val, (i % 9));
+        /*
+         * Using max bucket as (MAX_PRI - 1)
+         */
+        priqueue_put_data(p, val, (i % 9));
+        i++;
+
+        sleep(1);
+    }
+}
+
+void* consumer(void* p)
+{
+    sleep(2);
+    ptable *table = (ptable*)p;
+
+    char* val;
+    int priority;
+
+    printf("Thread consumer\n");
+    int i = 0;
+
+    while (1) {
+        printf("<--- get_data\n");
+        priqueue_get_data(p, &val, &priority);
+        printf("Search-> Priority = %d val = %s\n\n", priority, val);
+        free(val);
+
+        /*
+         * We break the consumer after dequeuing 16 messages.
+         * The next call to get_data will block since there
+         * will be no data from the producer
+         */
+        if (i == 15) {
+            break;
+        }
+    }
+}
+
+void cleanup(ptable *p)
+{
+    node *n = p->buf_pool;
+
+    while (n) {
+        node* temp = n;
+        n = n->next;
+        free(temp);
+    }
+
+    free(p);
+}
+
+int main()
+{
+    ptable *p = malloc(sizeof(ptable));
+    priqueue_create(p);
+
+    pthread_t thread1, thread2;
+
+    int iret1, iret2;
+
+    iret1 = pthread_create(&thread1, NULL, producer, (void*) p);
+    iret2 = pthread_create(&thread2, NULL, consumer, (void*) p);
+
+    priqueue_display(p);
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    cleanup(p);
+    printf("cleanup \n");
+}
+#endif
